93872a78980812827bf6a7ae2f94039967f2d1d1
XyuzalKiller <189678824+XyuzalKiller@users.noreply.github.com>
2025-12-07 06:41:38 +0700

    misc: update

--- plugins/DataSaver/src/main/kotlin/com/github/razertexz/Main.kt
+++ plugins/DataSaver/src/main/kotlin/com/github/razertexz/Main.kt
@@ -1,19 +1,25 @@
 package com.github.razertexz
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
+import com.aliucord.Utils
 
 import com.discord.api.user.User
 import com.discord.api.guildmember.GuildMember
+import com.discord.api.message.embed.EmbedAuthor
 import com.discord.api.guild.Guild
 import com.discord.api.role.GuildRole
 
 import de.robv.android.xposed.XC_MethodHook
 
 @AliucordPlugin(requiresRestart = false)
-class Main : Plugin() {
+internal class Main : Plugin() {
+    private val cm = Utils.appContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
     init {
         settingsTab = SettingsTab(PluginSettings::class.java).withArgs(settings)
     }
@@ -21,6 +27,8 @@ class Main : Plugin() {
     override fun start(ctx: Context) {
         setNull(User::class.java, "a", "userAvatars")
         setNull(GuildMember::class.java, "b", "userAvatars")
+        setNull(EmbedAuthor::class.java, "b", "userAvatars")
+
         setNull(User::class.java, "b", "userBanners")
         setNull(GuildMember::class.java, "c", "userBanners")
 
@@ -32,7 +40,7 @@ class Main : Plugin() {
     private fun setNull(clazz: Class<*>, methodName: String, key: String) {
         patcher.patch(clazz, methodName, emptyArray(), object : XC_MethodHook() {
             override fun beforeHookedMethod(param: XC_MethodHook.MethodHookParam) {
-                if (!settings.getBool(key, false)) {
+                if (cm.getNetworkCapabilities(cm.activeNetwork)?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true && !settings.getBool(key, false)) {
                     param.result = null
                 }
             }
