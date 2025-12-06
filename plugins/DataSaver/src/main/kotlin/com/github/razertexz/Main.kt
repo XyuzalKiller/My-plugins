c033a17da3b15a73fce571b9433fca70a9aaedb9
XyuzalKiller <189678824+XyuzalKiller@users.noreply.github.com>
2025-12-06 19:05:30 +0700

    misc: update

--- plugins/DataSaver/src/main/kotlin/com/github/razertexz/Main.kt
+++ plugins/DataSaver/src/main/kotlin/com/github/razertexz/Main.kt
@@ -4,14 +4,13 @@ import android.content.Context
 
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
-import com.aliucord.api.PatcherAPI
-
-import de.robv.android.xposed.XC_MethodHook
 
 import com.discord.api.user.User
 import com.discord.api.guildmember.GuildMember
 import com.discord.api.guild.Guild
 import com.discord.api.role.GuildRole
+
+import de.robv.android.xposed.XC_MethodHook
 
 @AliucordPlugin(requiresRestart = false)
 class Main : Plugin() {
@@ -20,19 +19,18 @@ class Main : Plugin() {
     }
 
     override fun start(ctx: Context) {
-        patcher.setNull(User::class.java, "a", "userAvatars")
-        patcher.setNull(GuildMember::class.java, "b", "userAvatars")
+        setNull(User::class.java, "a", "userAvatars")
+        setNull(GuildMember::class.java, "b", "userAvatars")
+        setNull(User::class.java, "b", "userBanners")
+        setNull(GuildMember::class.java, "c", "userBanners")
 
-        patcher.setNull(User::class.java, "b", "userBanners")
-        patcher.setNull(GuildMember::class.java, "c", "userBanners")
-
-        patcher.setNull(Guild::class.java, "q", "serverIcons")
-        patcher.setNull(Guild::class.java, "e", "serverBanners")
-        patcher.setNull(GuildRole::class.java, "d", "roleIcons")
+        setNull(Guild::class.java, "q", "serverIcons")
+        setNull(Guild::class.java, "e", "serverBanners")
+        setNull(GuildRole::class.java, "d", "roleIcons")
     }
 
-    private fun PatcherAPI.setNull(clazz: Class<*>, methodName: String, key: String) {
-        patch(clazz, methodName, emptyArray(), object : XC_MethodHook() {
+    private fun setNull(clazz: Class<*>, methodName: String, key: String) {
+        patcher.patch(clazz, methodName, emptyArray(), object : XC_MethodHook() {
             override fun beforeHookedMethod(param: XC_MethodHook.MethodHookParam) {
                 if (!settings.getBool(key, false)) {
                     param.result = null
